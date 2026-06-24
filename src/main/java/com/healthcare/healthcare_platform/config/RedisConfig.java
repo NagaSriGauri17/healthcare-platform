e5db168a6ffb051ecdbc1471cec4c7c898778b63
Naga Sri Gauri <srigauri12@gmail.com>
2026-06-25 03:29:12 +0530

    Fix Redis URL parsing for Railway deployment

--- src/main/java/com/healthcare/healthcare_platform/config/RedisConfig.java
+++ src/main/java/com/healthcare/healthcare_platform/config/RedisConfig.java
@@ -1,14 +1,36 @@
 package com.healthcare.healthcare_platform.config;
 
+import io.lettuce.core.RedisURI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.url:redis://localhost:6379}")
+    private String redisUrl;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisURI redisURI = RedisURI.create(redisUrl);
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisURI.getHost());
+        config.setPort(redisURI.getPort());
+
+        if (redisURI.getPassword() != null && redisURI.getPassword().length > 0) {
+            config.setPassword(new String(redisURI.getPassword()));
+        }
+
+        return new LettuceConnectionFactory(config);
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
