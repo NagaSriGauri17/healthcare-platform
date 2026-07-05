77346b3ec93ad0560649e36d9ba7e36562db7eb1
Naga Sri Gauri <srigauri12@gmail.com>
2026-07-05 10:22:24 +0530

    Add @Primary to RedisConfig bean and debug logging

--- src/main/java/com/healthcare/healthcare_platform/config/RedisConfig.java
+++ src/main/java/com/healthcare/healthcare_platform/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.healthcare.healthcare_platform.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -19,9 +20,12 @@ public class RedisConfig {
     private String redisUrl;
 
     @Bean
+    @Primary
     public RedisConnectionFactory redisConnectionFactory() {
+        System.out.println("=== RedisConfig: Building connection factory with URL: " + redisUrl + " ===");
         try {
             URI uri = new URI(redisUrl);
+            System.out.println("=== Parsed host: " + uri.getHost() + ", port: " + uri.getPort() + " ===");
 
             RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
             config.setHostName(uri.getHost());
@@ -37,6 +41,7 @@ public class RedisConfig {
 
             return new LettuceConnectionFactory(config);
         } catch (Exception e) {
+            System.out.println("=== RedisConfig ERROR: " + e.getMessage() + " ===");
             throw new RuntimeException("Failed to parse Redis URL: " + redisUrl, e);
         }
     }
